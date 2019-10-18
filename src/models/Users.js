758fe3e8eea0a1e0a6451583ceeaca8b3d596e97
Joselito Lima <joselitolima90@hotmail.com>
2019-10-18 20:03:22 -0300

    Mudanca no Model de usuário

--- src/models/Users.js
+++ src/models/Users.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const UsersSchema =  new mongoose.Schema({
     username: { type: String, required: true },
     fullname: { type: String, required: true },
-    function: { type: String, required: true },
     gerency: { type: String, required: true }
 });
 
